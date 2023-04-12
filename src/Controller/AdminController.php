aee93a51440c8cbd646e754b8c7c8dfb11078c77
akkacha94 <ali.akkacha15@outlook.fr>
2023-04-12 10:45:31 +0200

    create dashboard view + access control

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,35 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
-#[Route('/amdin')]
+#[Route('/admin')]
 class AdminController extends AbstractController
 {
     #[Route('/tableau-de-bord', name: 'show_dashboard', methods: ['GET'])]
     public function showDashboard(EntityManagerInterface $entityManager): Response
-    # Désactiver access_control dans config/packages/security.yaml !! (sinon cela ne fonctionne pas.)
     {
-
+        # Ce bloc de code try/catch() permet de bloquer l'accès et de rediriger si le rôle n'est pas bon.
+        # Désactiver access_control dans config/packages/security.yaml !! (sinon cela ne fonctionne pas.)
         try {
             $this->denyAccessUnlessGranted("ROLE_ADMIN");
-
-        } catch (AccessDeniedException $exeption ) {
-            $this->addFlash('danger', "cette partie du site est réservée");
-            return $this->redirectToRoute(('app_login'));
-            
+        } catch (AccessDeniedException $exception) {
+            $this->addFlash('danger', "Cette partie du site est réservée.");
+            return $this->redirectToRoute('app_login');
         }
 
-        $categories = $entityManager->getRepository(Category::class)->findBy(['deletedAt'=> null]);
+        $categories = $entityManager->getRepository(Category::class)->findBy(['deletedAt' => null]);
 
-        
-        return $this->render('admin/show_dasboard.html.twig', [
+        return $this->render('admin/show_dashboard.html.twig', [
             'categories' => $categories
         ]);
-
-    }
-}
+    } // end showDashboard()
+} // end class
